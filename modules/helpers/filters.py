e358d23f43c91a423cfde67b15141b5fff7bf621
creatorPavan <93319387+creatorPavan@users.noreply.github.com>
2022-05-09 20:57:35 +0530

    Update filters.py

--- modules/helpers/filters.py
+++ modules/helpers/filters.py
@@ -1,1 +1,11 @@
+from pyrogram import filters
+from typing import List, Union
+from modules.config import COMMAND_PREFIXES
+
+other_filters = filters.group & ~ filters.via_bot & ~ filters.forwarded
+other_filters2 = filters.private & ~ filters.via_bot & ~ filters.forwarded
+
+
+def command(commands: Union[str, List[str]]):
+    return filters.command(commands, COMMAND_PREFIXES)
 
